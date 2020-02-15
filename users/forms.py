cd8f0441203b90fe6ce952b1942f6a906acdc0c2
Leung Wing Man <vaniceleung@Leungs-MacBook-Air.local>
2020-02-15 16:26:07 +0800

    Login with email

--- users/forms.py
+++ users/forms.py
@@ -2,8 +2,13 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
-
 class UserRegisterForm(forms.Form):
     username = forms.CharField(max_length=30)
     email = forms.EmailField()
     password = forms.CharField(max_length=32) 
+
+
+class UserLoginForm(forms.Form): 
+    email = forms.EmailField() 
+    # password = forms.PasswordInput() 
+    password = forms.CharField(widget=forms.PasswordInput())
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from . import forms
 from django.contrib.auth.models import User
-# Create your views here.
+from django.contrib.auth import authenticate, login
 
 
 def home(request):
@@ -16,11 +16,32 @@ def register(request):
             email = form.cleaned_data.get('email')
             password = form.cleaned_data.get('password')
             user = User.objects.create_user(username=username, email=email)
-            
+
             user.set_password(password)
-            user.save() 
+            user.save()
             return redirect('home')
-    else: 
-        form = forms.UserRegisterForm() 
+    else:
+        form = forms.UserRegisterForm()
 
     return render(request, 'users/register.html', {'form': form})
+
+
+def Login(request):
+    if request.method == 'POST':
+        form = forms.UserLoginForm(request.POST)
+        if form.is_valid():
+            form_email = form.cleaned_data.get('email')
+            form_password = form.cleaned_data.get('password')
+            theusername = User.objects.get(email=form_email).username
+
+            user = authenticate(
+                request, username=theusername, password=form_password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                print("Invalid Data")
+
+    else:
+        form = forms.UserLoginForm()
+    return render(request, 'users/login.html', {'form': form})
