25a9c65b5222be90b2b145a59b4bbe6615274825
Leung Wing Man <vaniceleung@Leungs-MacBook-Air.local>
2020-02-15 18:19:28 +0800

    Working fine

--- users/forms.py
+++ users/forms.py
@@ -2,13 +2,27 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
+
 class UserRegisterForm(forms.Form):
-    username = forms.CharField(max_length=30)
-    email = forms.EmailField()
-    password = forms.CharField(max_length=32) 
+    username = forms.CharField(max_length=30, label='')
+    email = forms.EmailField(label='')
+    password = forms.CharField(
+        max_length=32, label='', widget=forms.PasswordInput())
+
+    def __init__(self, *args, **kwargs):
+        super(UserRegisterForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['placeholder'] = ('username')
+        self.fields['email'].widget.attrs['placeholder'] = ('email')
+        self.fields['password'].widget.attrs['placeholder'] = ('password')
 
 
-class UserLoginForm(forms.Form): 
-    email = forms.EmailField() 
-    # password = forms.PasswordInput() 
-    password = forms.CharField(widget=forms.PasswordInput())
+class UserLoginForm(forms.Form):
+    email = forms.EmailField(label='')
+    # password = forms.PasswordInput()
+    password = forms.CharField(widget=forms.PasswordInput(), label='')
+
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+
+        self.fields['email'].widget.attrs['placeholder'] = ('email')
+        self.fields['password'].widget.attrs['placeholder'] = ('password')
